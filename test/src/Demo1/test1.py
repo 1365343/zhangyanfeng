3ea4eb3d84f5ac29aa87c44b1a05088391c11c96
zhangyanfeng <zhangyanfeng>
2017-02-21 11:22:35 +0800

    test

---
+++ test/src/Demo1/test1.py
@@ -0,0 +1,63 @@
+'''
+Created on 2017年29
+
+@author: zhangyanfeng
+'''
+
+
+
+
+counter = 100          # 整型变量
+miles   = 1000.0       # 浮点型变量
+name    = "runoob"     # 字符串
+
+
+a,b,c = 1,2,"test";
+
+a=1
+b=a
+c=b
+
+
+del a
+del b
+
+a,b=1,3
+print(a+b,a*b,a/b,a//b,a%b,a**b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
